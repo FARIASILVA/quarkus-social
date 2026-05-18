42966d748f4f6dcc9b658ca3e73f2d8deb0009a4
FARIASILVA <92648160+FARIASILVA@users.noreply.github.com>
2026-05-18 15:06:43 -0300

    mais alterações

--- src/main/java/io/github/dougllasfps/quarkussocial/domain/model/User.java
+++ src/main/java/io/github/dougllasfps/quarkussocial/domain/model/User.java
@@ -1,7 +1,7 @@
 package io.github.dougllasfps.quarkussocial.domain.model;
 
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
